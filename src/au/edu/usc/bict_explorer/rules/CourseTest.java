151fbcfeceaa3a7f08155013eda45ceb5f58752c
Kanish Gosain <kanishgo@gmail.com>
2018-05-07 16:45:49 +0530

    Added Test class for Course

---
+++ src/au/edu/usc/bict_explorer/rules/CourseTest.java
@@ -0,0 +1,34 @@
+package au.edu.usc.bict_explorer.rules;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void isOffered() {
+    }
+
+    @Test
+    void isSatisfied() {
+    }
+
+    @Test
+    void getPreReqs() {
+    }
+
+    @Test
+    void getSemesters() {
+    }
+}
